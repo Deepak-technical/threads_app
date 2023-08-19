91ff224519116f1d87d386027ff8656bb4aaf5de
Deepak-technical <deepakakash156@gmail.com>
2023-08-19 22:38:16 +0530

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page = async() => {
+  return (
+    <div><section>
+        <h1 className='head-text mb-10'>Community</h1></section></div>
+  )
+}
+
+export default Page
